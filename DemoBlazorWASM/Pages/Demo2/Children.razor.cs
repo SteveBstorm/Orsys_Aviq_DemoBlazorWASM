1e8159acca8bc4c9dd50cec3dae718cebd2d679f
stevebstorm <steve.lorent@gmail.Com>
2024-01-31 12:14:20 +0100

    ajout pdf

---
+++ DemoBlazorWASM/Pages/Demo2/Children.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazorWASM.Pages.Demo2
+{
+    public partial class Children
+    {
+        [Parameter]
+        public int ValueFromParent { get; set; }
+
+        [Parameter]
+        public EventCallback<string> MyEvent { get; set; }
+
+        public string MyString { get; set; }
+
+        public async Task Send()
+        {
+           await MyEvent.InvokeAsync(MyString);
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            ValueFromParent *= 2;
+        }
+    }
+}
---
+++ DemoBlazorWASM/Pages/Demo2/Demo2.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace DemoBlazorWASM.Pages.Demo2
+{
+    public partial class Demo2
+    {
+        public int MyValue { get; set; } = 10;
+
+        public string ValueFromChildren { get; set; }
+        public void ReceiveFromChildren(string s)
+        {
+            ValueFromChildren = s;
+        }
+
+        public void Increment()
+        {
+            MyValue++;
+           
+        }
+
+        protected override bool ShouldRender()
+        {
+            if (MyValue == 15) return false;
+            return true;
+        }
+
+        public Demo2()
+        {
+            Console.WriteLine("Contructeur");
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            await Console.Out.WriteLineAsync("Passage dans la méthode Initialized");
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Console.WriteLine("Premier rendu => chargement");
+            }
+            else
+            {
+                Render++;
+                Console.WriteLine("Rendu nr " + Render);
+            }
+            
+        }
+
+        public int Render { get; set; }
+    }
+}
