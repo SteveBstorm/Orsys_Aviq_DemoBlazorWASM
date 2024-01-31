32e0febea7b1e28a4b4be89f9966e5e1ddf2bf21
stevebstorm <steve.lorent@gmail.Com>
2024-01-31 10:17:55 +0100

    Ajoutez des fichiers projet.

---
+++ DemoBlazorWASM/Pages/Demo1/Demo1.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazorWASM.Pages.Demo1
+{
+    public partial class Demo1
+    {
+        public int Value { get; set; } = 42;
+    
+        public void Increment()
+        {
+            Value++;
+        }
+
+        public void Decrement(int nb)
+        {
+            Value -= nb;
+        }
+    }
+}
